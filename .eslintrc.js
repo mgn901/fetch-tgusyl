71dfa660d420b10d2bd1c726641497b8d35396d6
mgn901 <mgn901+dev@gmail.com>
2023-05-17 02:38:33 +0900

    Installed ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['airbnb-base', 'airbnb-typescript/base'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json'
+  },
+  rules: {
+  }
+}
