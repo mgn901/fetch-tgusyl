8a468fff71aaf6d0fbd18de34f073f3f14a249fb
mgn901 <mgn901+dev@gmail.com>
2023-05-16 16:44:58 +0900

    Add build scripts for WebExtension

---
+++ scripts/build.esbuild.ts
@@ -0,0 +1,29 @@
+import { BuildOptions } from 'esbuild';
+import { build } from 'esbuild';
+import copyPlugin from 'esbuild-copy-static-files';
+
+const NODE_ENV = process.env.NODE_ENV ?? 'development';
+const isDev = NODE_ENV === 'development';
+
+const extensionBuildOptions: BuildOptions = {
+	assetNames: 'assets/[name]',
+	bundle: true,
+	define: {
+		'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+	},
+	entryPoints: ['./src/webextension/index.ts'],
+	minify: isDev,
+	sourcemap: isDev,
+	outdir: `./${NODE_ENV}/webextension`,
+	target: ['chrome110', 'firefox110', 'edge110', 'safari14'],
+	plugins: [
+		copyPlugin({
+			src: './src/webextension/static',
+			dest: `./${NODE_ENV}/webextension`,
+		}),
+	],
+};
+
+(async () => {
+	await build(extensionBuildOptions);
+})();
