189c41a2de57bc28aa6c7d528ee96795ec29432d
mgn901 <mgn901+dev@gmail.com>
2023-05-17 18:04:26 +0900

    Add test for convert*.*.ts

---
+++ src/test-data/subjectCompactListSample.ts
@@ -0,0 +1,126 @@
+import { ISubjectCompact } from '../types/ISubjectCompact';
+
+export const tabKamokuSampleHTML = `<style type="text/css">
+</style>
+
+<div style='font-size:10px; text-align: right;padding-right:30px;'>※各項目をクリックすると項目順に並べ替えを行います。</div>
+<table style='width:650px;background:white;' class='tbl' cellspacing='0' cellpadding='5' id='tblead'>
+  <thead id='disphead'>
+    <tr data-href='none' data-parm='year=2023&fcode=11&dcode=000&ccode=000&tcode=' class='tblHeadR'
+      style='color:white; background:#682187;'>
+      <th data-sort='SUBJECT_CODE' style='width:50px;'>科目CD</th>
+      <th data-sort='SUBJECT_NAME' style='width:230px;'>科目</th>
+      <th data-sort='GRADE' style='width:30px;'>学年</th>
+      <th data-sort='CLASS_NAME' style='width:40px;'>クラス</th>
+      <th data-sort='SEMESTER_NAME' style='width:40px;'>学期</th>
+      <th data-sort='WEEK_CODE' style='width:40px;'>曜日・時限</th>
+      <th data-sort='ROOM_NAME' style='width:50px;'>講義室</th>
+      <th data-sort='TEACHER_NAME' style='width:60px;'>担当教員</th>
+      <th data-sort='MEMO' style='width:60px;'>備考</th>
+    </tr>
+  </thead>
+</table>
+<div class='tblScroll'>
+  <table style='width:700px;background:white;' class='tbl tblBody' cellspacing='0' cellpadding='5'>
+    <thead id='printhead'>
+      <tr data-href='none' data-parm='year=2023&fcode=11&dcode=000&ccode=000&tcode=' class='tblHeadR'
+        style='color:white; background:#682187;'>
+        <th data-sort='SUBJECT_CODE' style='width:50px;'>科目CD</th>
+        <th data-sort='SUBJECT_NAME' style='width:230px;'>科目</th>
+        <th data-sort='GRADE' style='width:30px;'>学年</th>
+        <th data-sort='CLASS_NAME' style='width:40px;'>クラス</th>
+        <th data-sort='SEMESTER_NAME' style='width:40px;'>学期</th>
+        <th data-sort='WEEK_CODE' style='width:40px;'>曜日・時限</th>
+        <th data-sort='ROOM_NAME' style='width:50px;'>講義室</th>
+        <th data-sort='TEACHER_NAME' style='width:60px;'>担当教員</th>
+        <th data-sort='MEMO' style='width:60px;'>備考</th>
+      </tr>
+    </thead>
+    </tbody>
+    <tr data-href='https://tgusyl.example.com/ext_syllabus/referenceDirect.do?nologin=on&subjectID=example0&formatCD=1'>
+      <td style='width:50px;'>70000000</td>
+      <td style='width:230px;'>example科目名0</td>
+      <td style='width:30px;text-align: center;'>1</td>
+      <td style='width:40px;text-align: center;'>０１</td>
+      <td style='width:40px;'>春学期</td>
+      <td style='width:40px;'>月1</td>
+      <td style='width:50px;'>Ｓ１０１</td>
+      <td style='width:60px;'>example教員0、example教員1</td>
+      <td style='width:50px;'>example備考</td>
+    </tr>
+    <tr data-href='https://tgusyl.example.com/ext_syllabus/referenceDirect.do?nologin=on&subjectID=example0&formatCD=1'>
+      <td style='width:50px;'>70000000</td>
+      <td style='width:230px;'>example科目名0</td>
+      <td style='width:30px;text-align: center;'>1</td>
+      <td style='width:40px;text-align: center;'>０１</td>
+      <td style='width:40px;'>春学期</td>
+      <td style='width:40px;'>月2</td>
+      <td style='width:50px;'>Ｓ１０１</td>
+      <td style='width:60px;'>example教員0、example教員1</td>
+      <td style='width:50px;'>example備考</td>
+    </tr>
+    <tr data-href='https://tgusyl.example.com/ext_syllabus/referenceDirect.do?nologin=on&subjectID=example1&formatCD=1'>
+      <td style='width:50px;'>70000001</td>
+      <td style='width:230px;'>example科目名1</td>
+      <td style='width:30px;text-align: center;'>1</td>
+      <td style='width:40px;text-align: center;'>０１</td>
+      <td style='width:40px;'>春学期</td>
+      <td style='width:40px;'>火1</td>
+      <td style='width:50px;'>Ｓ１０１</td>
+      <td style='width:60px;'>example教員0、example教員1</td>
+      <td style='width:50px;'>example備考</td>
+    </tr>
+    <tr data-href='https://tgusyl.example.com/ext_syllabus/referenceDirect.do?nologin=on&subjectID=&formatCD=1'>
+      <td style='width:50px;'>70000002</td>
+      <td style='width:230px;'>example科目名2</td>
+      <td style='width:30px;text-align: center;'>1</td>
+      <td style='width:40px;text-align: center;'>０１</td>
+      <td style='width:40px;'>春学期</td>
+      <td style='width:40px;'>&nbsp;</td>
+      <td style='width:50px;'>&nbsp;</td>
+      <td style='width:60px;'>example教員0、example教員1</td>
+      <td style='width:50px;'>example備考</td>
+    </tr>
+    </tbody>
+  </table>
+</div>
+`;
+
+export const subjectCompactListSample: ISubjectCompact[] = [
+  {
+    id: 'example0',
+    code: '70000000',
+    name: 'example科目名0',
+    teachers: ['example教員0', 'example教員1'],
+    grades: [1],
+    classes: ['01'],
+    places: ['S101'],
+    semesters: ['春学期'],
+    weeks: ['月1', '月2'],
+    updatedAt: 1680307200,
+  },
+  {
+    id: 'example1',
+    code: '70000001',
+    name: 'example科目名1',
+    teachers: ['example教員0', 'example教員1'],
+    grades: [1],
+    classes: ['01'],
+    places: ['S101'],
+    semesters: ['春学期'],
+    weeks: ['火1'],
+    updatedAt: 1680307200,
+  },
+  {
+    id: '7000000201',
+    code: '70000002',
+    name: 'example科目名2',
+    teachers: ['example教員0', 'example教員1'],
+    grades: [1],
+    classes: ['01'],
+    places: [],
+    semesters: ['春学期'],
+    weeks: [],
+    updatedAt: 1680307200,
+  },
+];
---
+++ src/test-data/subjectDetailSample.ts
@@ -0,0 +1,331 @@
+import { ISubjectDetail } from '../types/ISubjectDetail';
+
+export const tgusylReferenceDirectSampleHTML = `<html lang="ja-JP">
+
+<head>
+  <title>
+    シラバス参照
+  </title>
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+  <SPAN ID="timerDisplay" STYLE="font-size:0;font-color:white;"></SPAN>
+  <table width="100%" border="0" cellpadding="0" cellspacing="10">
+    <tr>
+      <td class="txt12" colspan="2">
+      </td>
+    </tr>
+    <tr>
+      <td class="txt12" valign="top">
+        <table width="100%" border="1" cellspacing="0" cellpadding="0" bgcolor="#999999">
+          <tr bgcolor="#FFFFFF">
+            <td>
+              <table border="0" cellspacing="10" cellpadding="0">
+                <tr>
+                  <td class="txt12" nowrap>
+                    タイトル「<font color="#FF0000"><b>example年度 example学部シラバス</b></font>」、フォルダ「<font color="#FF0000">
+                      <b>example年度 example学部シラバス－exampleディレクトリ</b>
+                    </font>」<br>
+                    シラバスの詳細は以下となります。<br>ナンバリングは科目コードとは異なります。ご注意ください。
+                  </td>
+                </tr>
+                <tr>
+                  <td class="txt12Info" nowrap>
+                    <form name="Syllabus2Form" method="post" action="/ext_syllabus/aboutNumbering.do">
+                      <input type="hidden" id="aboutNumbering">
+                      <a href="#"
+                        onclick="return dbPrintClick('print', document.getElementById('aboutNumbering'));">ナンバリングとは</a>
+                    </form>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+  <SPAN ID="timerDisplay" STYLE="font-size:0;font-color:white;"></SPAN>
+  <table width="100%" border="0" cellpadding="0" cellspacing="10">
+    <tr>
+      <td>
+        <table border="1" cellspacing="0" cellpadding="3" class="txt12" bgcolor="#999999" align="left">
+          <tr bgcolor="#FFFFFF">
+            <td class="txt12" BGCOLOR="#6699FF" WIDTH="170" nowrap>
+              <font color="#FFFFFF">
+                科目コード&nbsp;
+              </font>
+            </td>
+            <td class="txt12" width="770" colspan="3">
+              70000000&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td class="txt12" BGCOLOR="#6699FF" WIDTH="170" nowrap>
+              <font color="#FFFFFF">
+                科目名&nbsp;
+              </font>
+            </td>
+            <td class="txt12" width="770" colspan="3">
+              example科目名&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td class="txt12" bgcolor="#6699ff" WIDTH="170" nowrap>
+              <font color="#FFFFFF">
+                担当教員&nbsp;
+              </font>
+            </td>
+            <td class="txt12" width="770" colspan="3">
+              example教員0,example教員1&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td class="txt12" bgcolor="#6699FF" width="170" nowrap>
+              <font color="#FFFFFF">
+                対象学年&nbsp;
+              </font>
+            </td>
+            <td class="txt12" width="770" colspan="3">
+              1年&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td class="txt12" bgcolor="#6699FF" width="170" nowrap>
+              <font color="#FFFFFF">
+                クラス&nbsp;
+              </font>
+            </td>
+            <td class="txt12" width="300">
+              ０１&nbsp;
+            </td>
+            <td class="txt12" bgcolor="#6699FF" width="170" nowrap>
+              <font color="#FFFFFF">
+                講義室&nbsp;
+              </font>
+            </td>
+            <td class="txt12" width="300">
+              Ｓ１０１ &nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td class="txt12" bgcolor="#6699FF" width="170" nowrap>
+              <font color="#FFFFFF">
+                開講学期&nbsp;
+              </font>
+            </td>
+            <td class="txt12" width="300">
+              春学期&nbsp;
+            </td>
+            <td class="txt12" bgcolor="#6699FF" width="170" nowrap>
+              <font color="#FFFFFF">
+                開講時期&nbsp;
+              </font>
+            </td>
+            <td class="txt12" width="300">
+              春学期&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td class="txt12" bgcolor="#6699FF" width="170" nowrap>
+              <font color="#FFFFFF">
+                曜日・時限&nbsp;
+              </font>
+            </td>
+            <td class="txt12" width="300">
+              月１&nbsp;
+            </td>
+            <td class="txt12" bgcolor="#6699FF" width="170" nowrap>
+              <font color="#FFFFFF">
+                単位区分&nbsp;
+              </font>
+            </td>
+            <td class="txt12" width="300">
+              必修&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td class="txt12" bgcolor="#6699FF" width="170" nowrap>
+              <font color="#FFFFFF">
+                授業形態&nbsp;
+              </font>
+            </td>
+            <td class="txt12" width="300">
+              講義・演習&nbsp;
+            </td>
+            <td class="txt12" bgcolor="#6699FF" width="170" nowrap>
+              <font color="#FFFFFF">
+                単位数&nbsp;
+              </font>
+            </td>
+            <td class="txt12" width="300">
+              2&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td class="txt12" BGCOLOR="#6699FF" WIDTH="170" nowrap>
+              <font color="#FFFFFF">受講対象&nbsp;</FONT>
+            </td>
+            <td class="txt12" width="770" colspan="3">
+              example課程 example区分0、example課程 example区分1&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td class="txt12" BGCOLOR="#6699FF" WIDTH="170" nowrap>
+              <font color="#FFFFFF">備考&nbsp;</FONT>
+            </td>
+            <td class="txt12" width="770" colspan="3">
+              example備考&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td class="txt12" BGCOLOR="#6699FF" WIDTH="170" nowrap>
+              <font color="#FFFFFF">
+                ナンバリング&nbsp;
+              </font>
+            </td>
+            <td class="txt12" width="770" colspan="3">
+              133E54&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td nowrap bgcolor="#99FFCC" WIDTH="170">
+              <font color="#0000FF">ねらいと目標</font>
+            </td>
+            <td width="770" colspan="3">
+              exampleねらいと目標&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td nowrap bgcolor="#99FFCC" WIDTH="170">
+              <font color="#0000FF">内容</font>
+            </td>
+            <td width="770" colspan="3">
+              example内容&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td nowrap bgcolor="#99FFCC" WIDTH="170">
+              <font color="#0000FF">テキスト</font>
+            </td>
+            <td width="770" colspan="3">
+              exampleテキスト<BR />exampleテキスト&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td nowrap bgcolor="#99FFCC" WIDTH="170">
+              <font color="#0000FF">参考文献</font>
+            </td>
+            <td width="770" colspan="3">
+              example参考文献&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td nowrap bgcolor="#99FFCC" WIDTH="170">
+              <font color="#0000FF">成績評価方法</font>
+            </td>
+            <td width="770" colspan="3">
+              example成績評価方法<BR />example成績評価方法&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td nowrap bgcolor="#99FFCC" WIDTH="170">
+              <font color="#0000FF">授業スケジュール（展開計画）</font>
+            </td>
+            <td width="770" colspan="3">
+              example授業スケジュール&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td nowrap bgcolor="#99FFCC" WIDTH="170">
+              <font color="#0000FF">授業時間外における学習方法</font>
+            </td>
+            <td width="770" colspan="3">
+              example授業時間外における学習方法&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td nowrap bgcolor="#99FFCC" WIDTH="170">
+              <font color="#0000FF">授業のキーワード</font>
+            </td>
+            <td width="770" colspan="3">
+              exampleキーワード&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td nowrap bgcolor="#99FFCC" WIDTH="170">
+              <font color="#0000FF">受講補足（履修制限等）</font>
+            </td>
+            <td width="770" colspan="3">&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td nowrap bgcolor="#99FFCC" WIDTH="170">
+              <font color="#0000FF">学生へのメッセージ</font>
+            </td>
+            <td width="770" colspan="3">
+              example学生へのメッセージ&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td nowrap bgcolor="#99FFCC" WIDTH="170">
+              <font color="#0000FF">実務経験のある教員による科目</font>
+            </td>
+            <td width="770" colspan="3">&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td nowrap bgcolor="#99FFCC" WIDTH="170">
+              <font color="#0000FF">授業実施上の特記事項</font>
+            </td>
+            <td width="770" colspan="3">
+              example授業実施上の特記事項&nbsp;
+            </td>
+          </tr>
+          <tr bgcolor="#FFFFFF">
+            <td nowrap bgcolor="#99FFCC" WIDTH="170">
+              <font color="#0000FF">その他 </font>
+            </td>
+            <td width="770" colspan="3">&nbsp;
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+  <table width="630" border="0" cellspacing="0" cellpadding="0" align="center">
+    <tr>
+      <td align="center" height="20"></td>
+    </tr>
+    <tr>
+      <td align="center" class="txt10" nowrap>
+        <font color="#666666">Copyright(C) example.</font>
+      </td>
+    </tr>
+  </table>
+  <br>
+</body>
+
+</html>
+`;
+
+export const subjectDetailSample: ISubjectDetail = {
+  id: 'example',
+  code: '70000000',
+  name: 'example科目名',
+  teachers: ['example教員0', 'example教員1'],
+  grades: [1],
+  classes: ['01'],
+  places: ['S101'],
+  semesters: ['春学期'],
+  weeks: ['月1'],
+  categories: ['example課程 example区分0', 'example課程 example区分1'],
+  updatedAt: 1680307200,
+  units: 2,
+  properties: [
+    { key: 'メモ', type: 'plain', value: 'example備考' },
+    { key: 'ナンバリング', type: 'plain', value: '133E54' },
+    { key: 'テキスト', type: 'plain', value: 'exampleテキスト\nexampleテキスト' },
+    { key: '出典', type: 'plain', value: '[シラバス参照 - 東京学芸大学 授業ガイド https://example.com/]' },
+  ],
+};
