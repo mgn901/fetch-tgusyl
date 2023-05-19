778e83e3c4e7f5b83bc7bf32e120d3f592f1de81
mgn901 <mgn901+dev@gmail.com>
2023-05-19 13:04:12 +0900

    Change: Build popup and background scripts separately

--- scripts/build.esbuild.ts
+++ scripts/build.esbuild.ts
@@ -1,29 +1,31 @@
-import { BuildOptions } from 'esbuild';
-import { build } from 'esbuild';
+import { BuildOptions, build } from 'esbuild';
 import copyPlugin from 'esbuild-copy-static-files';
 
 const NODE_ENV = process.env.NODE_ENV ?? 'development';
 const isDev = NODE_ENV === 'development';
 
 const extensionBuildOptions: BuildOptions = {
-	assetNames: 'assets/[name]',
-	bundle: true,
-	define: {
-		'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-	},
-	entryPoints: ['./src/webextension/index.ts'],
-	minify: isDev,
-	sourcemap: isDev,
-	outdir: `./${NODE_ENV}/webextension`,
-	target: ['chrome110', 'firefox110', 'edge110', 'safari14'],
-	plugins: [
-		copyPlugin({
-			src: './src/webextension/static',
-			dest: `./${NODE_ENV}/webextension`,
-		}),
-	],
+  assetNames: 'assets/[name]',
+  bundle: true,
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+  },
+  entryPoints: [
+    './src/webextension/background.ts',
+    './src/webextension/action.ts',
+  ],
+  minify: isDev,
+  sourcemap: isDev,
+  outdir: `./${NODE_ENV}/webextension`,
+  target: ['chrome110', 'firefox110', 'edge110', 'safari14'],
+  plugins: [
+    copyPlugin({
+      src: './src/webextension/static',
+      dest: `./${NODE_ENV}/webextension`,
+    }),
+  ],
 };
 
 (async () => {
-	await build(extensionBuildOptions);
+  await build(extensionBuildOptions);
 })();
