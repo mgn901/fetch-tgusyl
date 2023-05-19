c5c5e2d7c9213dd84441637838320285f637b4ab
mgn901 <mgn901+dev@gmail.com>
2023-05-19 13:03:07 +0900

    Fix: Include build.esbuild.ts to tsconfig.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,12 @@ module.exports = {
     node: true,
   },
   extends: ['airbnb-base', 'airbnb-typescript/base'],
-  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: 'tsconfig.json',
   },
   rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.test.ts', './scripts/**/*'] }],
   },
 };
