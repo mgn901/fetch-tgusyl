8ddeef0c1fa2f7d55b3c5dfb959dc779f6538ddb
mgn901 <mgn901+dev@gmail.com>
2025-04-14 23:06:54 +0000

    chore: introduce drizzle and libsql

---
+++ drizzle.config.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/schema.drizzle.ts',
+  dialect: 'sqlite',
+});
